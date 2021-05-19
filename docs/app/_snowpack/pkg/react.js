2c92222fd3e68bd6bd99445bbeb23f333da08a57
Carl Anderson <carl.anderson@microsoft.com>
2021-05-19 00:29:43 -0500

    static site for app

---
+++ docs/app/_snowpack/pkg/react.js
@@ -0,0 +1,9 @@
+import { r as react } from './common/index-ae389540.js';
+export { r as default } from './common/index-ae389540.js';
+
+
+
+var useCallback = react.useCallback;
+var useEffect = react.useEffect;
+var useState = react.useState;
+export { useCallback, useEffect, useState };
