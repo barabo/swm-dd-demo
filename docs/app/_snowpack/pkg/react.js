32126aa388f234f169d08f867b4e879971482cca
Carl Anderson <carl.anderson@microsoft.com>
2021-05-19 09:31:02 -0500

    wip: one more try, resetting contents of docs/app

--- docs/app/_snowpack/pkg/react.js
+++
@@ -1,9 +0,0 @@
-import { r as react } from './common/index-ae389540.js';
-export { r as default } from './common/index-ae389540.js';
-
-
-
-var useCallback = react.useCallback;
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useCallback, useEffect, useState };
