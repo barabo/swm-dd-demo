3607794c56585328a4f1526cffcf1806e2bccbce
Carl Anderson <carl.anderson@microsoft.com>
2021-05-24 16:48:40 -0500

    feat: deploy script for new versions of the EHR

---
+++ ehr/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Publishes a new instance of the app demo.
+#
+set -e
+set -E
+set -u
+
+trap 'echo -e "\nFAILED TO DEPLOY\n\\O/\n Y\n/ \\"' ERR
+
+echo "Checking lint..."
+npm run lint
+
+echo "Ensuring that configuration is not set to localhost...";
+grep '^const defaultAppUrl = ' src/Ehr.jsx | grep -v localhost
+
+echo "Copying built page into /docs..."
+cp -av build/ehr ../docs
+
+echo "Inserting the missing URL paths in /docs/ehr/index.html..."
+sed -i .bak -E '/ (href|src)=.\/ehr/s:"/ehr:"/swm-c10n-demo/app:' \
+  ../docs/ehr/index.html \
+  && rm -f ../docs/ehr/index.html.bak
+
+echo "Checking for changes to publish..."
+git status ../docs/ehr | grep -q 'nothing to commit' && echo "NOTHING NEW" && exit 0
+
+echo "Publishing built ehr to GHPages..."
+git add ../docs/ehr
+git commit -m "auto deployed by ehr/deploy.sh"
+git push
+
+echo -e '\nDEPLOY COMPLETE!!\n O/\n<Y\n/ >'
