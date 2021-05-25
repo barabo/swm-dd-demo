ee6c9c56d5af2e1e688140d537402052712a8370
Carl Anderson <carl.anderson@microsoft.com>
2021-05-25 14:40:36 -0500

    fix: fixes to the publish.sh helper

--- publish.sh
+++ publish.sh
@@ -8,8 +8,10 @@ function publish() {
   echo "Publishing $@... to branch $branch"
   git stash save
   git checkout $branch
+  git stash pop
   if git status "${@}" | grep -q 'nothing to commit'; then
     echo "NOTHING NEW"
+    git stash save
     git checkout -
     git stash pop
     exit 0
@@ -17,6 +19,7 @@ function publish() {
   git add "${@}"
   git commit -m "auto published by ${0}"
   git push
+  git stash save
   git checkout -
   git stash pop
   echo -e '\nDEPLOY COMPLETE!!\n O/\n<Y\n/ >'
