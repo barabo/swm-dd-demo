489b88370dd4175fb4720cce318f69e99c929a4e
Carl Anderson <carl.anderson@microsoft.com>
2021-05-27 18:31:32 -0500

    version bump to 0.1.4

---
+++ lib/build/prepublish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+set -u
+
+LATEST="$( npm show swm-client-lib version )"
+NEXT="$( grep -e '^[ ]*"version":' package.json | cut -d'"' -f4 )"
+
+# Make sure the version has been bumped before publishing.
+if [[ $NEXT == $LATEST ]]; then
+  echo "Please bump the version of the lib in package.json before publishing!"
+  exit 1
+fi
+
+HIGHEST="$( echo -e "$LATEST\n$NEXT" | sort -V | tail -n1 )"
+if [[ $HIGHEST == $LATEST ]]; then
+  echo "Detected a version regression!  Check npm and package.json!"
+  exit 1
+fi
+
+echo "OK to publish new version: $NEXT"
