cbb2b44d84ae74b2020df1ec15da541f0c879e61
Carl Anderson <carl.anderson@microsoft.com>
2021-05-28 14:22:15 -0500

    fix: prevented double-publication due to extra 'deploy' target in package.json

--- lib/build/prepublish.sh
+++ lib/build/prepublish.sh
@@ -3,18 +3,23 @@
 set -e
 set -u
 
+if [[ $( basename $PWD ) == 'lib' ]]; then
+  echo "Ensuring a fresh build..."
+  npm run build
+fi
+
 LATEST="$( npm show swm-client-lib version )"
 NEXT="$( grep -e '^[ ]*"version":' package.json | cut -d'"' -f4 )"
 
 # Make sure the version has been bumped before publishing.
 if [[ $NEXT == $LATEST ]]; then
-  echo "Please bump the version of the lib in package.json before publishing!"
+  echo "FAIL: Bump the version of the lib in package.json before publishing!"
   exit 1
 fi
 
 HIGHEST="$( echo -e "$LATEST\n$NEXT" | sort -V | tail -n1 )"
 if [[ $HIGHEST == $LATEST ]]; then
-  echo "Detected a version regression!  Check npm and package.json!"
+  echo "FAIL: Detected a version regression!  Check npm and package.json!"
   exit 1
 fi
 
